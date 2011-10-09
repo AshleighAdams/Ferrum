c9b162f9438beb5e35db4af1178f4f6c25cbf373
dzamkov <dzamkov@xiatek.org>
2011-10-09 18:17:09 -0400

    Using SFML, got empty window

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
-#include <iostream>
-
-using namespace std;
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	if( true )
-	{
-		lol;
-	}
+	sf::RenderWindow App(sf::VideoMode(800, 600, 32), "Ferrum");
+	
+	while (App.IsOpened())
+    {
+        sf::Event Event;
+        while (App.GetEvent(Event))
+        {
+            if ((Event.Type == sf::Event::Closed) || 
+               ((Event.Type == sf::Event::KeyPressed) && (Event.Key.Code == sf::Key::Escape)))
+            {
+                App.Close();
+                break;
+            }
+        }
+        
+        App.Clear();
+        App.Display();
+    }
     
-	return 0;
+    return 0;
 }
